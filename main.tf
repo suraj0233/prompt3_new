62e035a19826095ce085f03c9130b6f8ce10e639
suraj0233 <160613987+suraj0233@users.noreply.github.com>
2025-03-04 12:50:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,21 @@ terraform {
   }
 }
 
+variable "aws_account_id" {
+  description = "AWS Account ID for Spacelift integration"
+  type        = string
+}
+
+
 resource "spacelift_aws_integration" "aws_integration" {
   name = "aws-${var.aws_account_id}"
   
   role_arn = "arn:aws:iam::${var.aws_account_id}:role/Spacelift"
   
-  # Default region - can be made configurable if needed
+  
   default_region = "us-west-1"
   
   # Generate a unique stack ID for this integration
   stack_id = "aws-integration-${var.aws_account_id}"
 }
+
